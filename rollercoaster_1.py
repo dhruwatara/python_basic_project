b299ca4e4aca07a0a23a735323b5f33bb2bc702f
dhruwatara <82754676+dhruwatara@users.noreply.github.com>
2023-01-31 17:06:13 -0800

    Update rollercoaster_1.py

--- rollercoaster_1.py
+++ rollercoaster_1.py
@@ -4,4 +4,4 @@ height=int(input("what is your height in cm?"))
 if height>120:
     print("welcome, you can ride the rollercoaster")  
 else:
-    print("sorry you have to !!you can't ride")
+    print("sorry you have to grow taller, you can't ride")
