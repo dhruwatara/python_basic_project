690269d3cbdf96648bc014043837ef88e61d77f6
dhruwatara <82754676+dhruwatara@users.noreply.github.com>
2023-02-01 12:56:57 -0800

    Create rollercoaster_2.py

---
+++ rollercoaster_2.py
@@ -0,0 +1,11 @@
+print("welcome to the rollercoaster")
+
+height=int(input("what is your height in cm?"))
+if height <=120:
+    print("sorry you have to grow up taller, you cant ride!!")
+else:
+    age=int(input("what is your age?"))
+    if age>18:
+        print("you have to pay $12")
+    else:
+        print("ypu have to pay $7")
