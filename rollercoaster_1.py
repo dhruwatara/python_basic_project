c15b0d3db5f39cd3266dda90857190af51060a36
dhruwatara <82754676+dhruwatara@users.noreply.github.com>
2023-01-31 16:43:15 -0800

    Update rollercoaster_1.py

--- rollercoaster_1.py
+++ rollercoaster_1.py
@@ -1,6 +1,6 @@
 print("welcome to the rollarcoaster")
 
-height=int(input("what is your height in m?"))
+height=int(input("what is your height in cm?"))
 if height>120:
     print("welcome, you can ride the rollercoaster")  
 else:
